cb9eaa8239fc7f80f0270d676d16d2e75359aa7c
Adam <126939161+AdamSniadach@users.noreply.github.com>
2023-06-17 00:19:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import React from "react";
+// render html in react fist is what,second is where
+ReactDOM.render(
+  <div>
+    <h1> Hello world</h1>
+    <p>Thats paragraph </p>
+  </div>,
+  document.getElementById("root")
+);
